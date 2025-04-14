674ba0ccc659779d20df9b69d57a703005b36824
jatmn <the@jat.mn>
2025-04-14 09:37:04 -0700

    rebase to current release.

---
+++ data.lua
@@ -0,0 +1,10 @@
+
+require "util"
+
+require ("prototypes.subgroup")
+require ("prototypes.locomotive")
+require ("prototypes._wagon_cargo2a")
+require ("prototypes._wagon_cargo4a")
+
+
+
---
+++ prototypes/subgroup.lua
@@ -0,0 +1,4 @@
+
+data:extend({
+	{ type="item-subgroup", group="yuoki_railway", order="x", name="yir_americans", }, -- mod themed line
+})
